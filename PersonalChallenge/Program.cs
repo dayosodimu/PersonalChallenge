b3b02007d2475de7434faf42ff0c1e4fc2224672
desoulchild <33025335+dayosodimu@users.noreply.github.com>
2021-08-09 23:13:50 -0400

    Add project files.

---
+++ PersonalChallenge/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace PersonalChallenge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // store and retrieve data using literal and variable values 
+
+        }
+    }
+}
